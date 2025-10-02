e47ba549271b4ed483eab16d2f94aafbb3aa8554
duncanliao <duncanliaotio@gmail.com>
2025-10-02 13:57:10 +0800

    Update main5.c

--- EX3/main5.c
+++ EX3/main5.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main()
+{
+    int i=2100;
+    if(i%4==0){
+        if(i%100!=0){
+            printf("閏年");
+        }
+        else if(i%400==0){
+            printf("閏年");
+        }
+        else{
+            printf("不是閏年");
+        }
+    }    
+    return 0;
+}
