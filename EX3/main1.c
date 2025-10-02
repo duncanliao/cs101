596d7ceae461fac92a46f8d36aca68b3fc0ed5d6
duncanliao <duncanliaotio@gmail.com>
2025-10-02 14:51:13 +0800

    Update main1.c

--- EX3/main1.c
+++ EX3/main1.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void printsp(int i, int n){
+    for(int j = 0; j < n - i; j++){
+        printf(" ");
+    }
+}
+
+void printnum(int i){
+    for(int j = 0; j < i; j++){
+        printf("%d", i);
+        if(j < i - 1){
+            printf(" ");  
+        }
+    }
+}
+
+int main(){
+    int rows = 6;
+
+    for(int i = 1; i <= rows; i++){
+        printsp(i, rows);   
+        printnum(i);        
+        printf("\n");      
+    }
+
+    return 0;
+}
